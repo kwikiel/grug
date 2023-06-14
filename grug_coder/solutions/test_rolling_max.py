92062afc3d1ccdda502b10375f8ec9ff371a6c12
Kacper Wikiel <k.wikiel@gmail.com>
2023-06-14 16:14:30 +0200

    cleanup

--- grug_coder/solutions/test_rolling_max.py
+++
@@ -1,8 +0,0 @@
-from rolling_max import rolling_max
-def test_rolling_max():
-    assert rolling_max([1, 2, 3, 2, 3, 4, 2]) == [1, 2, 3, 3, 3, 4, 4]
-    assert rolling_max([4, 3, 2, 1]) == [4, 4, 4, 4]
-    assert rolling_max([1, 2, 3]) == [1, 2, 3]
-    assert rolling_max([3, 3, 3, 3]) == [3, 3, 3, 3]
-    assert rolling_max([1]) == [1]
-    assert rolling_max([]) == []
--- grug_coder/solutions/test_separate_paren_groups.py
+++
@@ -1,8 +0,0 @@
-from separate_paren_groups import separate_paren_groups
-def test_separate_paren_groups():
-    assert separate_paren_groups('( ) (( )) (( )( ))') == ['()', '(())', '(()())']
-    assert separate_paren_groups('()()()()') == ['()', '()', '()', '()']
-    assert separate_paren_groups('(())()') == ['(())', '()']
-    assert separate_paren_groups('()(()())()') == ['()', '(()())', '()']
-    assert separate_paren_groups('(()) ( ) (( )) (( )( ))') == ['(())', '()', '(())', '(()())']
-    assert separate_paren_groups('') == []
