773631b08c7a2289c66fc929d1a057c528ab983f
Kacper Wikieł <k.wikiel@gmail.com>
2023-06-19 15:45:42 +0200

    cleanup

--- grug_coder/test_ground_truth_has_close_elements.py
+++
@@ -1,6 +0,0 @@
-def check(string_to_md5):
-    # Check some simple cases
-    assert string_to_md5('Hello world') == '3e25960a79dbc69b674cd4ec67a72c62'
-    # Check some edge cases that are easy to work out by hand.
-    assert True
-check(string_to_md5)
